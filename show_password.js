02dba8214968a834be12e85fcd4b5a0737f74853
Lorem <578599945@qq.com>
2016-12-29 18:57:03 +0800

    加添【显示密码】 脚本
    对所有页面上的input[type=password]进行鼠标事件的监听，已达到鼠标移入显示密码（改为text）的效果。

---
+++ show_password.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         鼠标移入显示密码
+// @version      0.1
+// @description  鼠标移动到页面上的password密码框可以查看输入（临时改为text）
+// @author       fn
+// @require      https://cdn.bootcss.com/jquery/2.2.3/jquery.min.js
+// @include      *
+// ==/UserScript==
+
+(function () {
+    //使用严格模式
+    'use strict';
+
+    //获取所有password
+    var inputs = $("input[type=password]");
+
+    //遍历添加mouseoveer和mouseout事件改变type
+    inputs.map(function () {
+        var ele = $(this);
+        ele.mouseover(function () {
+            this.type = 'text';
+        }).mouseout(function () {
+            this.type = 'password';
+        });
+    });
+})();
