557ae197d1240f2f8a73b6c5f8bd22f8b4264759
Lorem <578599945@qq.com>
2016-12-30 10:47:13 +0800

    增加了 知乎日报精简 脚本。

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 ## 目前实现脚本
 1. 显示密码 (show_password.js)
+2. 知乎日报精简 (zhihu_daily_hidden.js)
---
+++ zhihu_daily_hidden.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         精简知乎日报页面
+// @version      0.1
+// @description  移除无用信息。
+// @author       fn
+// @require      https://cdn.bootcss.com/jquery/2.2.3/jquery.min.js
+// @include      *daily.zhihu.com*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    //隐藏头部
+    var header = $("div.header").css("display","none");
+    //隐藏横幅广告
+    var download = $("div.download").css("display","none");
+    //隐藏查看更多
+    var read_more = $("a.read-more").css("display","none");
+    //隐藏详情页顶部下载按钮
+    var buttons = $("a.button").css("display","none");
+    //隐藏详情页这篇文章有意思吗？
+    var question = $("div.question:last").css("display","none");
+    //隐藏详情页二维码
+    var qr = $("div.qr").css("display","none");
+
+    console.log("hidden success.");
+})();
