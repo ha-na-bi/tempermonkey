471c221ecb811fae4aac848d13c78f92bff6433f
fn <578599945@qq.com>
2016-12-30 10:59:03 +0800

    简化代码

--- show_password.js
+++ show_password.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         鼠标移入显示密码
-// @version      0.1
+// @version      0.2
 // @description  鼠标移动到页面上的password密码框可以查看输入（临时改为text）
 // @author       fn
 // @require      https://cdn.bootcss.com/jquery/2.2.3/jquery.min.js
@@ -11,16 +11,10 @@
     //使用严格模式
     'use strict';
 
-    //获取所有password
-    var inputs = $("input[type=password]");
-
-    //遍历添加mouseover和mouseout事件改变type
-    inputs.map(function () {
-        var ele = $(this);
-        ele.mouseover(function () {
+    //获取所有password,加入mouseover和mouseout事件
+    var inputs = $("input[type=password]").mouseover(function () {
             this.type = 'text';
         }).mouseout(function () {
             this.type = 'password';
         });
-    });
 })();
